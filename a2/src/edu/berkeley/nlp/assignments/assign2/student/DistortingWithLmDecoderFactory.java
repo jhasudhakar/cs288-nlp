237d677ae5aba3ec1f08a9b3f4f02102b28b9099
Reynold Xin <reynoldx@gmail.com>
2011-02-07 21:32:33 -0800

    Initial code for a2.

---
+++ a2/src/edu/berkeley/nlp/assignments/assign2/student/DistortingWithLmDecoderFactory.java
@@ -0,0 +1,18 @@
+package edu.berkeley.nlp.assignments.assign2.student;
+
+import edu.berkeley.nlp.langmodel.NgramLanguageModel;
+import edu.berkeley.nlp.mt.decoder.Decoder;
+import edu.berkeley.nlp.mt.decoder.DecoderFactory;
+import edu.berkeley.nlp.mt.decoder.DistortionModel;
+import edu.berkeley.nlp.mt.phrasetable.PhraseTable;
+
+public class DistortingWithLmDecoderFactory implements DecoderFactory
+{
+
+	public Decoder newDecoder(PhraseTable tm, NgramLanguageModel lm, DistortionModel dm) {
+
+		 return null;
+
+	}
+
+}
