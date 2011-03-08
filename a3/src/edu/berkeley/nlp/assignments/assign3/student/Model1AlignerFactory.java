f446fafdd22b660b4df718386cd9ea021edda1fa
Reynold Xin <reynoldx@gmail.com>
2011-03-08 15:12:49 -0800

    Imported assignment 3.

---
+++ a3/src/edu/berkeley/nlp/assignments/assign3/student/Model1AlignerFactory.java
@@ -0,0 +1,24 @@
+package edu.berkeley.nlp.assignments.assign3.student;
+
+import edu.berkeley.nlp.langmodel.NgramLanguageModel;
+import edu.berkeley.nlp.mt.SentencePair;
+import edu.berkeley.nlp.mt.WordAligner;
+import edu.berkeley.nlp.mt.WordAlignerFactory;
+import edu.berkeley.nlp.mt.decoder.Decoder;
+import edu.berkeley.nlp.mt.decoder.DecoderFactory;
+import edu.berkeley.nlp.mt.decoder.DistortionModel;
+import edu.berkeley.nlp.mt.phrasetable.PhraseTable;
+
+public class Model1AlignerFactory implements WordAlignerFactory
+{
+
+	public WordAligner newAligner(Iterable<SentencePair> trainingData) {
+
+
+
+
+
+		 return null;
+	}
+
+}
